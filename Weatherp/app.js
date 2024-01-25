44320d150c9e145988bf11528388b20a7c7247e1
Keshav Soni <74901427+keshavvsonii01@users.noreply.github.com>
2024-01-26 00:10:12 +0530

    Update Files.

--- Weatherp/app.js
+++ Weatherp/app.js
@@ -1,44 +1,51 @@
 const express = require("express");
 const https = require("https");
 const bodyParser = require("body-parser");
+const ejs = require("ejs");
 
 const app = express();
+app.set("view engine", "ejs");
 
-app.use(bodyParser.urlencoded({extented:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public")); // Serve static files from the 'public' directory
 
+app.get("/", function (req, res) {
+res.render("home")
+});
 
+app.post("/", function (req, res) {
+  const query = req.body.cityName;
+  const apiKey = "5e0bb6dd00624e8897fa615b77bef059";
+  const unit = "metric";
+  const url =
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    query +
+    "&units=" +
+    unit +
+    "&appid=" +
+    apiKey;
 
-app.get("/", function(req,res){
-    res.sendFile(__dirname + "/index.html");
-})
+  https.get(url, function (response) {
+    let body = "";
 
-app.post("/", function(req,res){
-    const query = req.body.cityName;
-    const apiKey = "5e0bb6dd00624e8897fa615b77bef059"
-    const unit = "metric"
-    const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query +"&units=" + unit+"&appid="+ apiKey
+    response.on("data", function (data) {
+      body += data;
+    });
 
-    https.get(url,function(response){
-        console.log(response)
+    response.on("end", function () {
+      const weatherData = JSON.parse(body);
+      const temp = weatherData.main.temp;
+      const description = weatherData.weather[0].description;
+      console.log(temp);
+      // Send data to the result page
+      res.render("result", {
+        temp: temp,
+        description: description,
+      });
+    });
+  });
+});
 
-        response.on("data", function(data){
-        const weatherData = JSON.parse(data)
-        const temp = weatherData.main.temp;
-        const description = weatherData.weather[0].description
-        console.log(temp);
-        console.log(description);
-        res.write("<h1>The temp - " + temp + "</h1>");
-        res.write("The description is " + description);
-        res.send();
-        })
-    })
-})
-
-
-
-
-
-
-app.listen(3000, function(){
-    console.log("Started!");
+app.listen(3000, function () {
+  console.log("Server started on port 3000");
 });
